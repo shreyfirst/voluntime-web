5b9891469436533e7b8019dbdceaedc223f83719
aryanm5 <aryanm.email@gmail.com>
2020-12-25 23:32:45 -0800

    changed loginToken and loginGoogle API path

--- src/services/login.js
+++ src/services/login.js
@@ -2,8 +2,8 @@ import { post } from './axiosConfig';
 
 const login = (info, callback) => post('/login', info, callback);
 
-const loginToken = (token, callback) => post('/loginToken', { token }, callback);
+const loginToken = (token, callback) => post('/login/token', { token }, callback);
 
-const loginGoogle = (token, callback) => post('/loginGoogle', { token }, callback);
+const loginGoogle = (token, callback) => post('/login/google', { token }, callback);
 
 export { login, loginToken, loginGoogle };
