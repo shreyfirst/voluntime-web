17d1d260d110e2e052d6ab018f4822db79d7c64a
aryanm5 <aryanm.email@gmail.com>
2021-04-01 19:53:56 -0700

    added Contact us text

--- src/components/dashboard/Contact.js
+++ src/components/dashboard/Contact.js
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <div>
-            Contact Us
+            If you have questions, concerns, or feedback, feel free to email me at <a href='mailto:aryan@mittaldev.com' target='_blank' rel='noopener noreferrer'>aryan@mittaldev.com</a>.
         </div>
     );
 };
